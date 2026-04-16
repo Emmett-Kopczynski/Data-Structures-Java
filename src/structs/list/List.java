227e5d37838ce043332c5d09d21db67b3f02a36d
Emmett-Kopczynski <emmettkopo@gmail.com>
2026-04-16 10:43:38 -0700

    added makefile, started on the list interface

---
+++ src/structs/list/List.java
@@ -0,0 +1,47 @@
+package  structs.list;
+
+
+//Java Library Imports
+import java.lang.IndexOutOfBoundsException;
+
+/**
+ * This is the List interface <br>
+ * Abstract Methods (See javadoc for more info on each one): <br>
+ * <br>
+ * void add(I toAdd) <br>
+ * void add(I toAdd, int index) <br>
+ * void clear() <br>
+ * boolean contains(I element) <br>
+ * <br>
+ * @author Emmett James Kopczynski (emmettkopo@gmail.com)
+ */
+public interface List<I> implements toString{
+
+
+    /**
+     * the add(I toAdd) method adds the given element to the end of the list
+     * @param toAdd - element to be added at the end of the list
+     */
+    public abstract void add(I toAdd);
+
+    /**
+     * the add(I toAdd, int index) method adds the given element at the given index
+     * @throws IndexOutOfBoundsException -- if the given index is not in the bounds of the list
+     */
+    public abstract void add(I toAdd, int index) throws IndexOutOfBoundsException;
+
+    /**
+     * the clear() method removes all elements in the list 
+     */
+    public abstract void clear();
+
+
+    /**
+     * the contains(I element) method returns true if the given argument is in the list, and false otherwise
+     * @param element - the element that we are looking for in the list
+     * @return - returns true if the element is in the list at least once, and false if it does not
+     */
+    public abstract boolean contains(I element); 
+
+
+}
